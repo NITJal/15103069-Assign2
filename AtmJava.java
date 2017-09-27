862f7ead38a3cee08b2294743caa99d0e16640c1
Mohit Meena <mmohit073@gmail.com>
2017-09-28 00:30:50 +0530

    Add files via upload

---
+++ AtmJava.java
@@ -0,0 +1,109 @@
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class AtmJava {
+	String AC;
+	int PIN;
+	int balance;
+	int check=0;
+	public AtmJava()
+	{
+		try
+		{ 
+			Scanner sc=new Scanner(System.in);
+			System.out.print("\nEnter your Registered Name:");
+			AC=sc.nextLine();
+			System.out.print("\nEnter your PIN number:");
+			PIN=sc.nextInt();
+		}
+		catch(InputMismatchException e)
+		{
+			System.out.print("\nEnter the valid input");
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.print("\nConversion not valid");
+		}
+	}
+	void deposit(int n)
+	{
+		this.balance+=n;
+	}
+	void withdrawl(int n)
+	{
+		this.balance-=n;
+	}
+	void display()
+	{
+		System.out.println("\nTotal Balance : "+this.balance);
+	}
+	public static void main(String args[])throws ClassNotFoundException
+	{
+		
+		Map<String,Integer> mp=new HashMap<String,Integer>(); 
+		mp.put("Mohit", 1234);
+		mp.put("Rahul", 5678);
+		mp.put("Neelam",9101);
+		mp.put("Rishi",1123);
+		
+		Scanner sc=new Scanner(System.in);
+		AtmJava obj=new AtmJava();
+		
+		try{
+			int am,amt;
+			for(Map.Entry<String,Integer> entry : mp.entrySet())
+			{
+				if( obj.AC.equals(entry.getKey()) && obj.PIN==entry.getValue())
+				{
+					obj.check++;
+					while(true)
+					{
+						System.out.println("\nEnter Your Choice: 1.Deposit 2.Withdrawl 3.Show Balance 4.Exit");
+						int n=sc.nextInt();
+						switch(n)
+						{
+						case 1:
+							System.out.print("\nAmount:");
+							am=sc.nextInt();
+							obj.deposit(am);
+							break;
+						case 2:
+							System.out.print("\nAmount:");
+							amt=sc.nextInt();
+							obj.withdrawl(amt);
+							break;
+						case 3:
+							obj.display();
+							break;
+						case 4:
+							System.exit(1);
+							break;
+						}
+					}
+					
+				}
+			}
+		}
+		catch(NullPointerException e)
+		{
+			System.out.print("\nNo operation is performed with null string");
+		}
+		catch(InputMismatchException e)
+		{
+			System.out.print("\nEnter the valid input");
+		}
+		catch(NoSuchElementException e)
+		{
+			System.out.print("\nElement not found");
+		}
+
+		if(obj.check==0)
+		{
+			//JOptionPane.showMessageDialog(null, "INVALID PIN NUMBER", "Warning",JOptionPane.WARNING_MESSAGE);
+			System.out.println("\nINVALID PIN NUMBER");
+		}
+	}
+}
---
+++ QStudentD.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader;
+	import java.io.IOException;
+	import java.io.InputStreamReader;
+	import java.util.InputMismatchException;
+	import java.util.Scanner;
+	
+public class QStudentD {
+	
+
+		String name;
+		String email;
+		long rn;
+		String address;
+		String ph;
+		double hsc;
+		double ug;
+		
+		public QStudentD() throws IOException
+		{
+			try{
+				InputStreamReader isr=new InputStreamReader(System.in);
+				BufferedReader br=new BufferedReader(isr);
+				System.out.println("Enter Roll No.:");
+				rn=Long.parseLong(br.readLine());
+				System.out.println("Enter Name:");
+				name=br.readLine();
+				while(true)
+				{
+					System.out.println("Enter Phone Number:");
+					ph=br.readLine();
+					if(ph.length()==10)
+					{
+						
+						break ;
+					}
+					System.out.println("*Invalid*");
+				}
+				while(true)
+				{
+					System.out.println("Enter Email id:");
+					email=br.readLine();
+					if(email.contains("@") && email.endsWith(".com"))
+					{
+						break ;
+					}
+					System.out.println("*Invalid*");
+				}
+				System.out.println("Enter Address:");
+				address=br.readLine();
+				System.out.println("Enter HSC Marks(%):");
+				hsc=Double.parseDouble(br.readLine());
+				System.out.println("Enter UG Marks(%):");
+				ug=Double.parseDouble(br.readLine());
+			}
+			catch(NumberFormatException e)
+			{
+				System.out.println("Caught "+e);
+			}
+			catch(InputMismatchException e)
+			{
+				System.out.println("Caught "+e);
+			}
+			catch(NullPointerException e)
+			{
+				System.out.println("Caught "+e);
+			}
+		}
+		void check()
+		{
+			if(hsc>=60 && ug>=70)
+			{
+				System.out.println("Eligible for National Institute");
+			}
+			else
+			{
+				System.out.println("Not Eligible for National Institute");
+			}
+		}
+		public static void main(String args[])
+		{
+			try{
+			QStudentD obj=new QStudentD();
+			obj.check();
+			}
+			catch(IOException e)
+			{
+				System.out.println("Caught "+e);
+			}
+		}
+}
